a1c0897819f8294999e699c03712d96ed6882f90
Diegodamata <Diegodamata>
2024-11-12 21:22:25 -0300

    GET game by id

--- src/main/java/com/diegodev/dslist/controllers/GameController.java
+++ src/main/java/com/diegodev/dslist/controllers/GameController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.diegodev.dslist.dto.GameDTO;
 import com.diegodev.dslist.dto.GameMinDTO;
 import com.diegodev.dslist.services.GameService;
 
@@ -22,6 +24,14 @@ public class GameController {
 	public ResponseEntity<List<GameMinDTO>> findAll(){
 		
 		List<GameMinDTO> list = gameService.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GameDTO> findById(@PathVariable Long id){
+		
+		GameDTO list = gameService.findById(id);
 		
 		return ResponseEntity.ok().body(list);
 	}
--- src/main/java/com/diegodev/dslist/services/GameService.java
+++ src/main/java/com/diegodev/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.diegodev.dslist.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.diegodev.dslist.dto.GameDTO;
 import com.diegodev.dslist.dto.GameMinDTO;
 import com.diegodev.dslist.entities.Game;
 import com.diegodev.dslist.repositories.GameRepository;
@@ -15,12 +17,24 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		
 		List<Game> list = gameRepository.findAll();
 		List<GameMinDTO> dto = list.stream().map(x -> new GameMinDTO(x)).toList();
 		
 		return dto ;
+	}
+	
+	//é uma boa pratica colocar essa anotação para que o operação com o banco seja concreta (ASID)
+	//Quando definido com readOnly = true, ela informa ao Spring que a operação não fará alterações no banco de dados
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		
+		Optional<Game> result = gameRepository.findById(id);
+		GameDTO dto = new GameDTO(result.get());
+		
+		return dto;
 	}
 
 }
